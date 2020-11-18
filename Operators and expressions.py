f8769be7efd8cf2bb75c5c0bec9b8f8461e4c18d
pankaj-cloud <65821916+pankaj-cloud@users.noreply.github.com>
2020-11-18 21:13:50 +0530

    Add files via upload

---
+++ Operators and expressions.py
@@ -0,0 +1,14 @@
+"""
+Your task is to prepare a simple code able to evaluate the end time of a period of time, given as a number of minutes (it could be arbitrarily large). The start time is given as a pair of hours (0..23) and minutes (0..59). The result has to be printed to the console.
+For example, if an event starts at 12:17 and lasts 59 minutes, it will end at 13:16.
+Don't worry about any imperfections in your code - it's okay if it accepts an invalid time - the most important thing is that the code produce valid results for valid input data.
+Test your code carefully.
+
+"""
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+a = ((mins + dura) // 60)
+b = ((mins + dura) % 60)
+c = (hour + a)
+print(c, b, sep=":" )
---
+++ Simple input and output.py
@@ -0,0 +1,15 @@
+# input a float value for variable a here
+# input a float value for variable b here
+
+# output the result of addition here
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+print("\nThat's all, folks!")
+value_a = float(input("Type a value for a :" ))
+value_b = float(input("Type a value for b :" ))
+print("The addition of a & b is ",value_a + value_b )
+print("The multiplication of a & b is ",value_a * value_b )
+print("The division of a & b is ",value_a / value_b )
+print("That's all, folks!")
